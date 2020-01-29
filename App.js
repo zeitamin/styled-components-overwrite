a37a74f5c645570b60d2c60d2f902f0b250dfeb4
zeitamin <pr33con@gmail.com>
2020-01-29 19:52:35 +0330

    overwrite styles

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import styled, { css } from "styled-components/native";
+
+const Col = styled(View)`
+  flex: 1;
+  background-color: orange;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Col
+      backgroundColor="yellow"
+      flex={0}
+      css={`
+        background: pink;
+      `}
+    >
+      <Text>aaa</Text>
+    </Col>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
